100b4d277206ac7cc29a0807eb287efb3c97b8b2
Vukasin Vranic <vukasinvranic@gmail.com>
2019-04-25 15:15:59 +0200

    Initial commit

---
+++ NerualNetworks/Brain/Neuron.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerualNetworks.Brain
+{
+    class Neuron
+    {
+        List<Dendrite> Dendrites { get; set; }
+        public Pulse OutputPulse { get; set; }
+        public double Weight;
+
+        public Neuron()
+        {
+            Dendrites = new List<Dendrite>();
+            OutputPulse = new Pulse();
+        }
+
+        public void Fire()
+        {
+            OutputPulse.Value = Sum();
+            OutputPulse.Value = Activation(OutputPulse.Value);
+        }
+
+        public void Compute (double learningRate, double delta)
+        {
+            Weight += learningRate * delta;
+            foreach(var terminal in Dendrites)
+            {
+                terminal.SynapticWeight = Weight;
+            }
+        }
+
+        private double Sum()
+        {
+            double computeValue = 0.0f;
+            foreach(var d in Dendrites)
+            {
+                computeValue += d.InputPulse.Value * d.SynapticWeight;
+            }
+
+            return computeValue;
+        }
+
+        private double Activation(double input)
+        {
+            double threshold = 1;
+            return input >= threshold ? 0 : threshold;
+        }
+    }
+}
---
+++ NerualNetworks/Brain/Pulse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerualNetworks.Brain
+{
+    class Pulse
+    {
+        public double Value { get; set; }
+    }
+}
